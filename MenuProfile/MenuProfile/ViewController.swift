2e24ee71a150a226a0127bc1fb00e4c75ad6496c
Alysson Menezes <almenezes912@gmail.com>
2022-12-07 18:18:50 -0300

    configurando projeto inicial  + navigation em viewCode

---
+++ MenuProfile/MenuProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuProfile
+//
+//  Created by ALYSSON DODO on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red 
+    }
+
+
+}
+
